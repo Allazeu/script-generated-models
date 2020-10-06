e68a64c9f8549df4dd72e99505a14d9aa39d7702
Centurian <38334600+Allazeu@users.noreply.github.com>
2020-10-06 13:32:09 -0400

    add portable_radar

---
+++ equipment-models/portable_radar/main.lua
@@ -0,0 +1,325 @@
+local Instance = loadstring(game:HttpGet("https://raw.githubusercontent.com/Allazeu/api/master/Lua/Roblox/BetterInstance.lua"))();
+
+local model = Instance.new('Model');
+
+local pv1 = Instance.new('Part', model, {
+	Name = "base" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.105882, 0.164706, 0.207843 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  true ,
+	Size = Vector3.new( 1.75, 0.0500000007, 1.75 ),
+	CFrame = CFrame.new( 11.5500002, 0.0249999966, 7.44999981, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+Instance.new('CylinderMesh', pv1, {
+	Scale = Vector3.new( 1, 1, 1 ),
+	Offset = Vector3.new( 0, 0, 0 ),
+	
+});
+local pv2 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.388235, 0.372549, 0.384314 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.399999976, 0.150000006, 0.199999958 ),
+	CFrame = CFrame.new( 11.5500002, 0.25, 6.94999981, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv3 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.639216, 0.635294, 0.647059 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.300000012, 0.300000012, 0.0500000007 ),
+	CFrame = CFrame.new( 11.2614784, 0.438210011, 7.03066969, 0.845820904, 0.266733468, 0.46199587, 0, 0.866025388, -0.5, -0.533466935, 0.422910452, 0.732502341 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv4 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.639216, 0.635294, 0.647059 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.300000012, 0.300000012, 0.0500000007 ),
+	CFrame = CFrame.new( 11.8231869, 0.437499791, 6.99136496, 0.923879504, -0.191341698, -0.331413507, 0, 0.866025388, -0.5, 0.382683396, 0.461939752, 0.800103068 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv5 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.639216, 0.635294, 0.647059 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.300000012, 0.300000012, 0.0500000007 ),
+	CFrame = CFrame.new( 11.7981806, 0.403210014, 7.90838575, 0.707106769, -0.353553385, 0.612372398, 0, 0.866025388, 0.5, -0.707106769, -0.353553385, 0.612372398 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv6 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.639216, 0.635294, 0.647059 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.300000012, 0.300000012, 0.0500000007 ),
+	CFrame = CFrame.new( 11.3590717, 0.402499765, 7.91092968, 0.707106769, 0.353553385, -0.612372398, 0, 0.866025388, 0.5, 0.707106769, -0.353553385, 0.612372398 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv7 = Instance.new('Part', model, {
+	Name = "body" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.388235, 0.372549, 0.384314 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 1, 0.349999994, 1 ),
+	CFrame = CFrame.new( 11.5500002, 0.200000003, 7.44999981, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+Instance.new('CylinderMesh', pv7, {
+	Scale = Vector3.new( 1, 1, 1 ),
+	Offset = Vector3.new( 0, 0, 0 ),
+	
+});
+local pv8 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.639216, 0.635294, 0.647059 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.300000012, 0.300000012, 0.0500000007 ),
+	CFrame = CFrame.new( 11.0810747, 0.458210021, 7.45391989, -4.37113883e-08, 0.5, -0.866025388, 0, 0.866025388, 0.5, 1, 2.18556941e-08, -3.78551732e-08 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv9 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.639216, 0.635294, 0.647059 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.300000012, 0.300000012, 0.0500000007 ),
+	CFrame = CFrame.new( 12.0566483, 0.437500209, 7.42137432, -4.37113883e-08, -0.5, 0.866025388, 0, 0.866025388, 0.5, -1, 2.18556941e-08, -3.78551732e-08 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv10 = Instance.new('Part', model, {
+	Name = "antenna" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.388235, 0.372549, 0.384314 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.0500000007, 1.14999998, 0.0500000007 ),
+	CFrame = CFrame.new( 11.5500002, 0.599999905, 7.94999981, 1, 0, 0, 0, 0.974752605, -0.223287016, 0, 0.223287016, 0.974752605 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+Instance.new('CylinderMesh', pv10, {
+	Scale = Vector3.new( 1, 1, 1 ),
+	Offset = Vector3.new( 0, 0, 0 ),
+	
+});
+local pv11 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.388235, 0.372549, 0.384314 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.0500000007, 0.0500000007, 0.0500000007 ),
+	CFrame = CFrame.new( 11.5500002, 1.15560865, 8.07727337, 1, 0, 0, 0, 0.974752605, -0.223287016, 0, 0.223287016, 0.974752605 ),
+	Shape =  Enum.PartType.Ball ,
+	
+});
+local pv12 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.388235, 0.372549, 0.384314 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.249999985, 0.0500000007, 0.0500000007 ),
+	CFrame = CFrame.new( 11.0250006, 0.245000005, 7.67499971, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv13 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.0666667, 0.0666667, 0.0666667 ),
+	Material =  Enum.Material.SmoothPlastic ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.5, 0.0500000007, 0.5 ),
+	CFrame = CFrame.new( 11.5500002, 0.360000014, 7.44999981, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+Instance.new('CylinderMesh', pv13, {
+	Scale = Vector3.new( 1, 1, 1 ),
+	Offset = Vector3.new( 0, 0, 0 ),
+	
+});
+local pv14 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.388235, 0.372549, 0.384314 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.249999985, 0.0500000007, 0.0500000007 ),
+	CFrame = CFrame.new( 11.0250006, 0.245000005, 7.2249999, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv15 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.388235, 0.372549, 0.384314 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.0500000007, 0.0500000007, 0.400000006 ),
+	CFrame = CFrame.new( 10.9250002, 0.245000005, 7.45000029, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv16 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.388235, 0.372549, 0.384314 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.249999985, 0.0500000007, 0.0500000007 ),
+	CFrame = CFrame.new( 12.0750008, 0.245000005, 7.67499971, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv17 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.388235, 0.372549, 0.384314 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.249999985, 0.0500000007, 0.0500000007 ),
+	CFrame = CFrame.new( 12.0750008, 0.245000005, 7.2249999, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv18 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.388235, 0.372549, 0.384314 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.0500000007, 0.0500000007, 0.400000006 ),
+	CFrame = CFrame.new( 12.1750002, 0.245000005, 7.45000029, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+local pv19 = Instance.new('Part', model, {
+	Name = "white" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.972549, 0.972549, 0.972549 ),
+	Material =  Enum.Material.SmoothPlastic ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.5, 0.0500000007, 0.5 ),
+	CFrame = CFrame.new( 11.5500002, 0.360000014, 7.44999981, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+Instance.new('CylinderMesh', pv19, {
+	Scale = Vector3.new( 0.99000001, 1.00999999, 0.99000001 ),
+	Offset = Vector3.new( 0, 0, 0 ),
+	
+});
+local pv20 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.105882, 0.164706, 0.207843 ),
+	Material =  Enum.Material.Metal ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 1.75, 0.100000001, 1.75 ),
+	CFrame = CFrame.new( 11.5500002, 0.0500000007, 7.44999981, 1, 0, 0, 0, 1, 0, 0, 0, 1 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+Instance.new('SpecialMesh', pv20, {
+	Scale = Vector3.new( 1, 1, 1 ),
+	Offset = Vector3.new( 0, 0, 0 ),
+	
+});
+local pv21 = Instance.new('Part', model, {
+	Name = "Part" ,
+	Anchored =  true ,
+	Color = Color3.new( 0.972549, 0.972549, 0.972549 ),
+	Material =  Enum.Material.Neon ,
+	Transparency =  0 ,
+	CanCollide =  false ,
+	Size = Vector3.new( 0.0500000007, 0.0500000007, 0.0500000007 ),
+	CFrame = CFrame.new( 11.5500002, 1.15560865, 8.07727337, 1, 0, 0, 0, 0.974752605, -0.223287016, 0, 0.223287016, 0.974752605 ),
+	Shape =  Enum.PartType.Block ,
+	
+});
+Instance.new('CylinderMesh', pv21, {
+	Scale = Vector3.new( 1.00999999, 0.0500000007, 11.0100002 ),
+	Offset = Vector3.new( 0, 0.00499999989, 0 ),
+	
+});
+
+local base = model:FindFirstChild('base');
+Instance.new('BodyGyro', base, {
+	MaxTorque = Vector3.new(400000, 0, 400000),
+	P = 30000,
+});
+
+for _, v in next, model:GetChildren() do
+	if (v ~= base) then
+		local weld = Instance.new('ManualWeld', base, {
+			C0 = base.CFrame:inverse() * v.CFrame,
+			Part0 = base,
+			Part1 = v,
+		});
+	end
+end
+
+local data = {
+	model = model,
+	base = base,
+	
+	objects = model:GetChildren(),
+};
+
+function data:clone()
+	local new = model:Clone();
+	for _, v in next, new do
+		v.Anchored = false;
+	end
+	
+	return new;
+end
+
+return data;
